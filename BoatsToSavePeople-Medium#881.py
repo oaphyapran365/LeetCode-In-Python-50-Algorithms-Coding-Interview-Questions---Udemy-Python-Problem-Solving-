bae2b06e819d1c8dd7834d9712e5f411816994af
Abdullahil-Oaphy <oaphyapran365@gmail.com>
2022-02-06 10:28:39 +0600

    commit

---
+++ BoatsToSavePeople-Medium#881.py
@@ -0,0 +1,27 @@
+# BoatsToSavePeople-Medium#881
+
+from typing import List
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+
+        left = 0
+        right = len(people)-1
+
+        boats_number = 0
+
+        while(left<=right):
+            if(left==right):
+                boats_number+=1
+                break
+            if(people[left]+people[right]<=limit):
+                left+=1
+
+            right-=1
+            boats_number+=1
+        return boats_number
+
+s = Solution()
+answer = s.numRescueBoats([2,1,3,4], 4)
+print(answer)
